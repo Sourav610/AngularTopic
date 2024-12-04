7910f919c1f19b89026120da9e154deec3972380
sonu kumar <souravk03221@outlook.com>
2024-12-05 00:18:24 +0530

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import {UserComponent} from './user/user.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [HeaderComponent,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
