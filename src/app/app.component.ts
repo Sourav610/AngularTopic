05965ee616971eb5d73fe61156201d0b24827f06
sonu kumar <souravk03221@outlook.com>
2024-12-31 20:00:23 +0530

    learn signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import {UserComponent} from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
-  title = 'first-angular-app';
+ users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
@@ -11,14 +11,25 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // uses for randomly selected user
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // imagePath = computed(() => 'assets/users/'+this.selectedUser().avatar);
+
+
+  // get imagePath(){
+  //   return 'assets/users/'+this.selectedUser.avatar // it will not work in signal
+  // }
 
   get imagePath(){
-    return 'assets/users/'+this.selectedUser.avatar
+    return 'assets/users/'+this.avatar // it will not work in signal
   }
 
+  @Input() avatar!:string;  // ! it tells typescript that it will definitely set some value as typescript will not able to find.
+  @Input() name!:string;
+
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
